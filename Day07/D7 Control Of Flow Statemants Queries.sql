911e138f4c04580e8e53133a2e5cc63254fbc20b
Rania Rabie Sayed <167593676+RaniaRabie@users.noreply.github.com>
2026-03-17 21:19:14 +0200

    Database Day07

---
+++ Day07/D7 Control Of Flow Statemants Queries.sql
@@ -0,0 +1,202 @@
+﻿use ITI
+
+
+---------------------------- >>> Control Of Flow Statements <<< ----------------------------
+-- if, if else => select مقدرش استخدمها مع
+declare @x int
+update Student
+set St_Age +=1
+
+select @x = @@ROWCOUNT
+if @x > 0
+	select 'Multiple Rows Affected'
+else 
+	select 'No rows Affected'
+
+	-- عادي جدا and, or  ممكن اعمل  
+	-- برضو عادي if else  ممكن كمان اعمل
+	-- {} زي البروجرامنج عندنا begin, end بنحط if تحت line لو عندي اكتر من
+
+DECLARE @Salary INT = 5000;
+DECLARE @Position VARCHAR(20) = 'Manager';
+
+if (@Salary > 4000 AND @Position = 'Manager')
+	BEGIN
+		select 'High salary manager';
+		select 'You can approve budgets';
+	END
+else
+    select 'Normal employee';
+
+
+-- iif => كويسة جدا performance  بنستخدمها لما يقي عندنا حالتين فقط وهيا ك 
+		  -- column فبالتالي النتيجة تظهر ك select بقدر استخدم مع
+select St_Id, Crs_Id, iif(grade >= 50 , 'Pass', 'Fail') as status
+from Stud_Course
+
+-- case => select مع column وعايزة اظهر النتيجة ك condition لو عندنا اكتر من
+select Ins_Name, Salary, 
+case
+	when Salary >= 10000 then 'Senior - No raise Needed'
+	when Salary >= 7000 then 'Mid-Level  - 10% raised'
+	when Salary >= 5000 then 'Senior - 15% raised'
+	else
+	'Entry level - 20% raised'
+end
+
+from Instructor
+
+-- update مع case ممكن كمان استخدم
+-- if else كامل زي  condition بيها مش update بنحط جواها القيم اللي هن
+/*
+	update Student
+	set St_Age +=1 XXXXX
+*/
+update Instructor
+set Salary = Salary *
+	case Ins_Degree
+	when 'PHD' then 1.20
+	when 'Master' then 1.10
+	else 1.05
+end
+
+-- if exists if not exists
+
+-- app ده بيضرب في error المشكلة ان ال error زي كده في احتمال انها تشتغل او تضرب query لو انا بعمل
+--   بالاسم ده ولا لا؟ table طب هو مفيش طريقة تعرفني هل عندنا
+-- run query قبل ما check بالاسم ده ولا لا عايزة اعمل table اسألها عندنا metadata بطريقة تانية عايزة اكلم
+create table Student(
+id int,
+name varchar(50)
+)
+
+-- sys schema => contains a collection of system catalog views, functions, and procedures
+--	     		 that provide metadata about the database and its objects.
+-- sys (system schema) => databse بتاع  metadata فيها ال tables, views فيها sys عندها database كل  
+
+
+-- اللي عندنا tables يعني هنا مثلا بنرجع كل اسامي
+select name from sys.tables 
+
+-- ومش هيرجع حاجة لو معندناش student table لو عندنا student ده هيرجع query نرجع للي كنا فيه ال
+-- false لا يبقي true بس انا مش مهتمة باللي هيرجه انا بس عايزة اعرف هيرجع حاجة ولا لا لو رجع حاجة يبقي 
+select name from sys.tables where name = 'student'
+
+-- عليها نعرف هيا موجودة ولا لا check هنحط جواها الحاجة اللي عايزين ن if exists() عشان كده يجي دور
+-- وبناء عليه هننفذ حاجة معينة
+if exists (select name from sys.tables where name = 'Student')
+	select 'Table with the same Name already exists' 
+else
+	create table Student(
+	id int,
+	name varchar(50)
+	)
+/*
+	زي كده ممكن ميشتغلش ليه؟؟  query غالبا برضو 
+	instructor,student table مع relationship عنده Department table لان
+	app ل error وانا مش عايزة برضو اطلع
+*/
+delete from Department where Dept_Id = 20
+
+-- الحل؟؟
+/*
+	relationship ليه معاهم Department اللي tables اللي عايزه امسحه ده موجود في ال dept_id لو check بعمل 
+	وعليه بقرر هنعمل اي
+*/
+if not exists( select Dept_Id from Student where Dept_Id = 20 )
+and not exists (select Dept_Id from Instructor where Dept_Id = 20 )
+	delete from Department where Dept_Id = 20
+else
+	select 'Table has relationship'
+
+-- عندنا حل كمان اسهل
+-- try, catch => هيجي منين error لو مش واضح معايا اوي ال
+begin try
+	delete from Department where Dept_Id = 20
+end try
+begin catch
+	select 'error'
+	-- لو عايزة اعرض شوية معلومات عن الايرور اللي حصل
+	select ERROR_LINE(), ERROR_NUMBER(), ERROR_MESSAGE()
+end catch
+
+-------------------------------------------------------------------------------------------
+-- ** loops
+--		 while
+declare @x int = 10
+while @x <= 20
+	begin 
+		set @x += 1
+		if @x = 14
+			continue
+		if @x = 16
+			break
+		select @x
+	end
+
+
+-- wait for
+
+-- Delay
+
+-- choose
+
+
+---------------------------------------------------------------------------------------------
+-- transaction
+------explicit tranaction
+create table parent(id int primary key)
+create table child (cid int foreign key references parent(id))
+
+-- from 124 - 127 => for now run as batch
+insert into parent values(1)
+insert into parent values(2)
+insert into parent values(3)
+insert into parent values(4)
+
+-- from 130 - 132 => for now run as batch
+insert into child values(1) -- 1 row affected
+insert into child values(5) -- error
+insert into child values(3) -- 1 row affected
+
+-- هنلاقي القيم اللي دخلت 1و3 عادي ومفيش تأثير بالايرور
+select * from child
+
+truncate table child
+
+-- from 10 - 12 => now run as tranaction
+begin transaction
+	insert into child values(1) -- 1 row affected
+	insert into child values(2) -- 1 row affected
+	insert into child values(3) -- 1 row affected
+rollback -- فاضي وده منطقي ما انت اللي قولتلهtable علي الرغم من ان كل القيم صح هتلاقي rollback لو حطيت 
+
+select * from child
+
+begin transaction
+	insert into child values(1) -- 1 row affected
+	insert into child values(5) -- error
+	insert into child values(3) -- 1 row affected
+commit -- ما انت اللي قولتله برضو table علي الرغم من ان حصل ايرور الا انه دخل البيانات ال commit لو حطيت 
+
+/*
+	صح commit, rollback يبقي السؤال المهم ازاي احط 
+	commit احنا عايزين لما الدنيا تبقي تمام نحط
+	rollback لو حصل مشكلة تبقي
+الحل؟؟				
+	try catch نعمل
+*/
+
+begin try
+	begin transaction
+		insert into child values(1) 
+		insert into child values(2)
+		insert into child values(3) 
+	commit
+end try
+begin catch
+	rollback
+end catch
+
+
+select * from child
---
+++ Day07/D7 Functions Queries.sql
@@ -0,0 +1,175 @@
+﻿use ITI
+-------------------------- >>> Functions <<< --------------------------
+-- Bulit in functions
+select GETDATE()
+
+select ISNULL(St_Fname, ' ') -- One Replacement
+from Student
+
+select coalesce(St_Fname,St_Lname, ' ') -- More Than One Replacement
+from Student
+ 
+select UPPER(St_Fname), LOWER(St_Lname)
+from Student
+
+select LEN(St_Fname), St_Lname
+from Student
+
+select MAX(St_Fname) -- Max ASCII
+from Student
+
+-- عايزة اعرض اطول اسم عندي
+select top(1) St_Fname
+from Student
+order by len(St_Fname) desc
+
+select POWER(Salary, 2)
+from Instructor
+
+select convert(varchar(20),getdate(), 101)
+select format(GETDATE(), 'dd-MM-yyyy')
+
+select DB_NAME()
+select SUSER_NAME()
+
+-- Windowing Functions
+
+select s.St_Id as sid, St_Fname as sname, Grade, Crs_Name as Cname 
+into grade
+from Student s, Stud_Course sc, Course c
+where s.St_Id = sc.St_Id and c.Crs_Id = sc.Crs_Id
+
+select * from grade
+
+/*
+	LAG(): Accesses data from row اللي قبلي.
+	LEAD(): Accesses data from row اللي بعدي.
+*/
+
+-- وجبت درجة اللي قبلي واللي بعد grade ب order عملت 
+select sname, grade,
+	LAG(grade) over(order by grade) as prev,
+	LEAD(grade) over(order by grade) as next
+from grade
+
+-- وجبت اسم اللي قبلي واللي بعد grade ب order عملت 
+select sname, grade,
+	LAG(sname) over(order by grade) as prev,
+	LEAD(sname) over(order by grade) as next
+from grade
+
+-- نفس اللي فوق بس جبتها لشخص معين
+select * from(
+select sname, grade,
+	LAG(sname) over(order by grade) as prev,
+	LEAD(sname) over(order by grade) as next
+from grade) as newTable
+where sname = 'Saly'
+
+-- اللي فوق كان بيجيب اسم او درجة اللي قبلي واللي بعدي queries  عندنا مشكلة ان في ال
+-- حتي لو مش نفس المادة... طب لا انا عايزة درجات اللي قبلي واللي بعدي لنفس المادة
+
+select sname, grade, Cname,
+	LAG(grade) over(partition by Cname order by grade) as prev,
+	LEAD(grade) over(partition by Cname order by grade) as next
+from grade
+
+
+/*
+	FIRST_VALUE(): Returns the first value in the window frame.
+	LAST_VALUE(): Returns the last value in the window frame.
+*/
+select sname, grade,Cname,
+	FIRST_VALUE(grade) over(order by grade),
+	LAST_VALUE(grade) over(order by grade Rows BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+from grade
+
+-- with partition
+select sname, grade,Cname,
+	FIRST_VALUE(grade) over(partition by Cname order by grade),
+	LAST_VALUE(grade) over(partition by Cname order by grade Rows BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+from grade
+
+
+--------------------------------------------------------------------------------
+-- User Defined Functions
+--	1. Scalar functions
+		-- ex1: بتاع الطالب ترجع اسمه id ابعتلها function عايزة اعمل
+
+-- declaration
+-- programmability >> Functions بلاقيها في
+-- schemaName.funName	<<	معينة schema  احطها جوه create fun ممكن وانا 
+create function getName(@id int)
+returns varchar(20)
+
+-- body
+begin
+	declare @name varchar(20)
+	select @name = St_Fname from Student where St_Id = @id
+	return @name
+end
+
+-- call function
+-- bulit in functions عشام ميفكرهاش واحدة من dbo لازم اكتب
+select dbo.getName(1)
+
+-------------------------------------------------------------------------------------------
+
+--	2. Inline Table functions
+	-- ex2: اللي جوه القسم بالمرتب السنوي بتاعهم instructors ترجع اسامي dept بتاخد رقم fun عايز اعمل 
+create function getInstructor(@did int)
+returns table 
+as 
+return
+(
+	select Ins_Name, Salary*12 as totalSalary
+	from Instructor
+	where Dept_Id = @did
+)
+
+-- call => because it return a table
+select * from getInstructor(10)
+-- عادي فممكن اعمل كده table اتعامل معاها ك
+select Ins_Name from getInstructor(10)
+-- او كده او اللي عايزه
+select sum(totalSalary) from getInstructor(10)
+
+--------------------------------------------------------------------------------------------
+
+--	3. Multi Statements Table Valued functions
+/*
+	   ex3: fname ترجعلي ليست ب ارقام الطلبة و fname ابعتلها fun عايزة اعمل
+			lname ترجعلي ليست ب ارقام الطلبة و lname ابعتلها  
+			fullName ترجعلي ليست ب ارقام الطلبة و fullName ابعتلها
+			وبناء عليه ينفذ حاجة معينة if statement معينة يعمل عليها keyword يعني من الاخر انا ببعت 
+*/
+
+create function getStuds(@format varchar(50))
+returns @t table (
+	id int,
+	name varchar(50)
+)
+as 
+begin
+	if @format = 'first'
+		insert into @t
+		select St_Id, St_Fname from Student
+
+	else if @format = 'last'
+		insert into @t
+		select St_Id, St_Lname from Student
+
+	else if @format = 'full'
+		insert into @t
+		select St_Id, St_Fname + ' ' + St_Lname from Student
+	return
+end
+
+select * from getStuds('full')
+
+
+-- Noteee: run time اللي جواها في query لاننا بنعرف ال functions جوه execute مينفعش اكتب 
+--		   بس select بيبقي جواها fun وال insert or update or delete فممكن يبقي جواها
+
+
+---------------------------------------------------------------------------------------------
