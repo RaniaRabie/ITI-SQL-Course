fde879d7127fcbf43336bef74757025b41f41f77
Rania Rabie Sayed <167593676+RaniaRabie@users.noreply.github.com>
2026-03-17 21:25:48 +0200

    Database Day08

---
+++ Day08/D8  rollup, cube, grouping sets, pivot, unpivot.sql
@@ -0,0 +1,192 @@
+﻿--rollup , cube  , grouping sets,  pivot,  unpivot
+
+use test
+
+			------------------ >>> rollup <<< ------------------
+
+create table sales
+(
+ProductID int,
+SalesmanName varchar(10),
+Quantity int
+)
+truncate table sales
+
+insert into sales
+values  (1,'ahmed',10),
+		(1,'khalid',20),
+		(1,'ali',45),
+		(2,'ahmed',15),
+		(2,'khalid',30),
+		(2,'ali',20),
+		(3,'ahmed',30),
+		(4,'ali',80),
+		(1,'ahmed',25),
+		(1,'khalid',10),
+		(1,'ali',100),
+		(2,'ahmed',55),
+		(2,'khalid',40),
+		(2,'ali',70),
+		(3,'ahmed',30),
+		(4,'ali',90),
+		(3,'khalid',30),
+		(4,'khalid',90)
+		
+select ProductID,SalesmanName,quantity
+from sales
+
+-- product ده هيجيبلي مجموع المبيعات لكل 
+select ProductID as X,sum(quantity) as "quantities"
+from sales
+group by ProductID
+
+-- كمان فيه مجموع المبيعات كلها row طب افرض عايزة ازود
+-- union ممكن ب 
+select ProductID as X,sum(quantity) as "quantities"
+from sales
+group by ProductID
+union all
+select 0,sum(quantity)
+from sales
+
+-- subquery او 
+select sum("quantities") from
+	(select ProductID as X,sum(quantity) as "quantities"
+	from sales
+	group by ProductID) as newTable
+	
+-- rollup او نعمل
+-- sum زيادة فيه row  هتعمل 
+-- تاني query اللي موجودة ف fun وتنفذ عليه نفس ال query هيا بتعمل اي بقي >> بتاخد الناتج بتاع
+select ProductID as X,sum(quantity) as "quantities"
+from sales
+group by rollup(ProductID)
+
+
+-- لكل الموظفين total qty فيه row لكل موظف وبعدين total qty دي مثلا تجيب ال
+select SalesmanName as Name,sum(quantity) as Qty
+from sales
+group by rollup(SalesmanName)
+
+
+Select isnull(Name,'Total'),Qty
+from ( 
+select SalesmanName as Name,sum(quantity) as Qty
+from sales
+group by rollup(SalesmanName)
+) as t
+
+select SalesmanName as Name,sum(quantity) as Qty
+from sales
+group by rollup(SalesmanName)
+		
+
+select SalesmanName as Name,Count(quantity) as Qty
+from sales
+group by rollup(SalesmanName)
+		
+
+--order by ProductID,SalesmanName
+select ProductID,sum(quantity) as "Quantities"
+from sales
+group by rollup(ProductID)
+
+-- مع كل موظف product لكل quantities هيجيب مجموع
+select ProductID,SalesmanName,sum(quantity) as "Quantities"
+from sales
+group by ProductID,SalesmanName
+
+-- هتعمل اي؟ << rollup(ProductID,SalesmanName) لو عملت
+-- total sum فيه row هيرجعلي 
+-- ProductID << الاول بس column علي fun وتنفذ نفس ال
+select ProductID,SalesmanName,sum(quantity) as "Quantities"
+from sales
+group by rollup(ProductID,SalesmanName)
+
+-- rollup(SalesmanName,ProductID) لو غيرت الترتيب
+-- لكل موظف لوحده sum لانه هيطلع 
+select SalesmanName,ProductID,sum(quantity) as "Quantities"
+from sales
+group by rollup(SalesmanName,ProductID)
+
+--------------------------------------------------------------------------------------------
+
+			------------------ >>> cube <<< ------------------
+
+ -- cube => علي الاثنين rollup  لو عايزاه يعمل 
+
+ --order by ProductID,SalesmanName
+select ProductID,SalesmanName,sum(quantity) as "Quantities"
+from sales
+group by cube(ProductID,SalesmanName)
+
+ --order by SalesmanName, ProductID
+select ProductID,SalesmanName,sum(quantity) as "Quantities"
+from sales
+group by cube(SalesmanName,ProductID)
+
+
+--------------------------------------------------------------------------------------------
+
+			------------------ >>> grouping sets <<< ------------------
+
+-- بس rollup العادية يتبقي اي؟ نواتجgroup by من cube لو طرحنا ناتج ال
+-- grouping sets => الثاني col علي rollup الاول و col علي roll up بتعمل group by بتلغي ال
+select ProductID,SalesmanName,sum(quantity) as "Quantities"
+from sales
+group by grouping sets(ProductID,SalesmanName)
+
+--------------------------------------------------------------------------------------------
+
+			------------------ >>> Pivot and Unpivot OLAP <<< ------------------
+
+--if u have the result of the previouse query
+select ProductID,SalesmanName,sum(quantity) as "Quantities"
+from sales
+group by SalesmanName,ProductID
+-- matrix عايز يطلع الناتج ك client اللي فوق دي سليم بس ال query تمام هو ناتج
+
+/*
+	pid		ahmed	ali		khalid	
+	1		35		145		30
+	2		70		90		70	
+	3		60		NULL	30	
+	4		NULL	170		90
+*/ 
+-- columns بقت  rows كانك قلبت
+-- Pivot => grop by + rotation for table
+SELECT *
+FROM sales 
+PIVOT (SUM(Quantity) FOR SalesmanName IN ([Ahmed],[Khalid],[ali])) as PVT
+
+SELECT *
+FROM sales 
+PIVOT (SUM(Quantity) FOR SalesmanName IN ([Ahmed],[Khalid])) as PVT
+
+SELECT *
+FROM sales 
+PIVOT (SUM(Quantity) FOR productid IN ([1],[2],[3],[4])) as PVT
+
+Select * from newpivot
+
+
+select * from newtable
+
+
+--how to get the table
+SELECT * FROM newtable 
+UNPIVOT (qty FOR salesname IN ([Ahmed],[Khalid],[Ali])) UNPVT
+
+
+execute('SELECT * FROM sales 
+PIVOT(SUM(Quantity) FOR SalesmanName IN (p1))')
+
+PVT
+
+alter proc p1
+as
+select distinct(salesmanname)
+from sales
+
+p1
+
---
+++ Day08/D8 Index, Table Types.sql
@@ -0,0 +1,107 @@
+﻿use ITI
+
+------------------------------------------------------------------------------------------
+
+								-- >>> Index <<< --
+
+-- مش هيشتغل ليه؟
+-- clustered index اتكريتله automatically فبالتالي table علي PK لان اوريدي عندنا 
+create clustered index myIndex
+on Student(St_Fname)
+
+-- 
+create nonclustered index myIndex
+on Student(St_Fname)
+
+-- PK		=>	Constraint	>>	 clustered Index
+-- unique	=>	Constraint	>>	 nonclustered Index
+
+-- 2 index ده table هينتج عن 
+-- clustered => PK(id), nonclustered => unique (age)
+create table test222
+(
+id int primary key,
+name varchar(50),
+age int unique
+)
+
+-- unique index => nonclustered index
+-- unique constraint او عليه unique تكون قيمه  unique index اللي هكريت عليه column لازم ال
+-- والا هيطلع ايرور زي كده لانه بيتطبق علي الداتا القيدمة والجديدة
+create unique index index4
+on Student(st_age)
+
+/*
+	 ؟index اللي المفروض اكريت عليها columns مين ال   
+	 index اللي هيعملو بيها سيرش كتير وتبدأ تعمل عليها columns من كلام الناس مين ال detect من الاول بتبقي عارف بتبدأ تanalysis في
+	 index هتساعدني اعرف الناس بتسيرش ب اي كتير ف ابدأ عليهم  tools
+	 - SQL Server Profilier
+	 - SQL Server Tuning Advisor
+*/ 
+
+select * from student
+
+------------------------------------------------------------------------------------------
+					
+							-- >>> Table Types <<< --
+-- physical table
+create table exam(
+	eid int,
+	edate date,
+	numberOfQ int
+)
+-- drop لو عملتله DB امتي يتشال من 
+drop table exam
+
+			--------------- ********************** ---------------
+
+/*
+	Local table => session based tables
+	note => session اسمها new query كل 
+	اللي واقف عليها session علي مستوي local بيكون tempdb ده هيتكريت جوه table ال 
+	تاني user مش هيقدر يشوفه لان ده بالنسبالنا select * from #exam وكتبت new query يعني لو عملت
+	ID طب السيرفر هيفرق بينهم وبين بعض ازاي؟ هيدي لكل واحد #exam اسمه table وممكن هو كمان يعمل 
+*/
+
+create table #exam(
+	eid int,
+	edate date,
+	numberOfQ int
+)
+/*
+	 ؟DB امتي يتشال من 
+		 >> drop لو عملتله
+		 >> sesstion قفلت 
+*/
+
+			--------------- ********************** ---------------
+
+-- Global table => shared tables
+
+/*
+	- tempdb بيتكريت جوه
+	- واحد بيكريته واي حد بيتدخل والشخص اللي مكريته موجود يقدر يتعامل معاه
+*/
+create table ##exam(
+	eid int,
+	edate date,
+	numberOfQ int
+)
+
+/*
+	 ؟DB امتي يتشال من 
+		 >> سواء اللي مكريته او اللي بيشتغل عليه disconnect لما كل الناس الموجودة علي السيرفر تعمل  
+		 >> drop
+*/
+
+
+			--------------- ********************** ---------------
+
+-- Variable table 
+/*
+	اللي بيرن جواها batch علي مستوي local 
+*/
+
+declare @t table(id int)
+
+------------------------------------------------------------------------------------------
